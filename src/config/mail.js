98f37b18203841cb13891d849083346a43592e87
gusflopes <gusflopes86@gmail.com>
2019-10-19 17:46:58 -0400

    feat: Nodemailer on AppointmentController.delete

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '3a22f59ea5e71c',
+    pass: '529a8ae5a63fd5',
+  },
+  default: {
+    from: 'GoBarber Team <noreply@gobarber.com>',
+  },
+};
